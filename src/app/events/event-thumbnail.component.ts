a7022e22012d2dc9d4bb932466a1cbfd16db1b2c
João Vitor Barbosa <contato.cavalcantebarbosa@gmail.com>
2021-02-21 11:39:25 -0300

    Styling Components

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -10,11 +10,14 @@ import { Component, Input } from "@angular/core";
       <div>Price: R\${{event.price}}</div>
       <div>
         <span>Location: {{event.location.address}}</span>
-        <span>&nbsp;</span>
-        <span>{{event.location.city}}, {{event.location.country}}</span>
+        <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
       </div>
     </div>
-  `
+  `,
+  styles: [`
+    .pad-left: { padding-left: 10px; }
+    .well div { color: #bbb; }
+  `]
 })
 export class EventThumbnailComponent {
   @Input() event: any;
