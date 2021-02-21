513ee6b1028d4b73f368803b360ee8ee98da05dd
João Vitor Barbosa <contato.cavalcantebarbosa@gmail.com>
2021-02-21 09:51:55 -0300

    Creating First Data-bound Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
 
 @NgModule({
   declarations: [
-    EventsAppComponent
+    EventsAppComponent,
+    EventsListComponent
   ],
   imports: [
     BrowserModule
