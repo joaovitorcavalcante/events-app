265d816fff123dab7aa4bbac655ab005645f6dd7
João Vitor Barbosa <contato.cavalcantebarbosa@gmail.com>
2021-02-26 19:55:36 -0300

    Adding new page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ToastrService } from './common/toastr.service';
 
 import { EventsAppComponent } from './events-app.component';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventService } from './events/shared/event.service';
@@ -13,6 +14,7 @@ import { NavBarComponent } from './nav/nav.component';
     EventsAppComponent,
     EventsListComponent,
     EventThumbnailComponent,
+    EventDetailsComponent,
     NavBarComponent
   ],
   imports: [
